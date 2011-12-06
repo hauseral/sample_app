28b74e24c6c8684d1be35e85792bbaf798e757c0
Alexandra Hauser <alexandra_ha@freenet.de>
2011-12-06 10:43:47 +0100

    Finished layout and routes

--- Dijkstra_Algorithmus.rb
+++ Dijkstra_Algorithmus.rb
@@ -1,20 +1,37 @@
-class Word < String
-  def allcapitals?
-    return (self == self.upcase)
-  end
+D[a]= 0
+D[i]= INFINITY
+for a!=i
+  MK = a
 end
 
-w=Word.new("absde")
 
-puts w.allcapitals?
+
+
+
+
+
+
+
+
+
+
+#class Word < String
+#  def allcapitals?
+#    return (self == self.upcase)
+#  end
+#end
+
+#w=Word.new("absde")
+
+#puts w.allcapitals?
 
 #y="ANDREAS"                # string kein word deshalb klasse nicht erkannt
 #puts y.allcapitals?
 
 
-y=Word.new("ANDREAS")
+#y=Word.new("ANDREAS")
 
-puts y.allcapitals?
+#puts y.allcapitals?
 
 #q= "HUHU"
 #puts q.blank?
